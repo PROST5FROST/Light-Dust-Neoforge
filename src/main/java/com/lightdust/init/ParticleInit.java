f6011de8b6188b0f1e00a86ea4b04f050968f26c
PROST5FROST <prostfrost1@gmail.com>
2026-03-17 22:04:04 +0200

    Trying to make it work 2

--- src/main/java/com/lightdust/init/ParticleInit.java
+++ src/main/java/com/lightdust/init/ParticleInit.java
@@ -1,16 +1,12 @@
 package com.lightdust.init;
 
 import com.lightdust.LightDust;
-import net.minecraft.client.particle.Particle;
 import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class ParticleInit {
     public static final DeferredRegister<ParticleType<?>> PARTICLES =
@@ -19,10 +15,10 @@ public class ParticleInit {
                     LightDust.MODID
             );
 
-    public static final DeferredHolder<Particle, Particle> DUST_PARTICLE = PARTICLES.register(
-            "dust_particle",
-            () -> new ParticleTypes();
-    );
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> DUST_PARTICLE =
+            PARTICLES.register(
+                    "dust_particle", () -> new SimpleParticleType(false)
+            );
 
     public static void register(IEventBus eventBus) {
         PARTICLES.register(eventBus);
